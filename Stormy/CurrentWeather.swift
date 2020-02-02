bb5babc08c39157e28886c629e6e5ce386f5297d
Francisco Ozuna <ozuna252@hotmail.com>
2020-02-02 17:29:13 -0500

    Created CurrentWeather file to hold data model for app.

---
+++ Stormy/CurrentWeather.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by Francisco Ozuna on 2/2/20.
+//  Copyright © 2020 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeather {
+    let temperature: Double
+    let humidity: Double
+    let precipProbability: Double
+    let summary: String
+    let icon: String
+}
