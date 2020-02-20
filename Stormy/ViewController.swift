010cfc5e65e05ddb0633dedbb1350e5159b1d8c0
Francisco Ozuna <ozuna252@hotmail.com>
2020-02-20 14:37:57 -0500

    Added unique darkSkyApiKey (which will be reset for security) and constructed URL.

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -17,10 +17,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentSummaryLabel: UILabel!
     @IBOutlet weak var refreshButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate let darkSkyApiKey = "c4b31fae784f652c98aab5578b86a9df"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let base = URL(string: "https://api.darksky.net/forecast/\(darkSkyApiKey)/")
+        let forecastUrl = URL(string: "37.8267,-122.4233", relativeTo: base)
         
         let currentWeather = CurrentWeather(temperature: 85.0, humidity: 0.8, precipProbability: 0.1, summary: "Hot!", icon: "clear-day")
         
